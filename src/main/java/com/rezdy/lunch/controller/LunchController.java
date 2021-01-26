765c6f7188f06db3fb933996f788632792089416
Gillon Manalastas <gillon.m@outlook.com>
2021-01-26 15:51:21 +1100

    changed /lunch endpoint to get mapping

--- src/main/java/com/rezdy/lunch/controller/LunchController.java
+++ src/main/java/com/rezdy/lunch/controller/LunchController.java
@@ -1,9 +1,9 @@
 package com.rezdy.lunch.controller;
 
-import com.rezdy.lunch.service.LunchService;
 import com.rezdy.lunch.dto.Recipe;
+import com.rezdy.lunch.service.LunchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +20,7 @@ public class LunchController {
         this.lunchService = lunchService;
     }
 
-    @PostMapping("/lunch")
+    @GetMapping("/lunch")
     public List<Recipe> getRecipes(@RequestParam(value = "date") String date) {
         return lunchService.getNonExpiredRecipesOnDate(LocalDate.parse(date));
     }
